2dc059ba296081f383582f113f1812b2557040d9
Abdul Rehman Gulshan <sabdulrehmangulshan@gmail.com>
2026-08-19 05:42:52 +0500

    feat: create book appointment api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,6 +8,7 @@
 // - Login user
 // - Get profile
 // - Update profile
+// - Book Appointments
 // - Get appointments   -> FUTURE / separate controller
 // - Cancel appointment -> FUTURE / separate controller
 // - Payment gateway    -> FUTURE
@@ -19,6 +20,8 @@ import bcrypt from "bcrypt";
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 import { v2 as cloudinary } from 'cloudinary'
+import doctorModel from "../models/doctorModel.js";
+import appointmentModel from "../models/appointmentModel.js";
 
 // REGISTER USER
 const registerUser = async (req, res) => {
@@ -282,4 +285,83 @@ const updateProfile = async (req, res) => {
     }
 };
 
-export { registerUser, loginUser, getProfile, updateProfile };
+// API TO BOOK APPOINTMENT
+const bookAppointment = async (req, res) => {
+    try {
+        const { docId, slotDate, slotTime } = req.body
+        // getting userId added by auth service to my request
+        const userId = req.userId;
+
+        const docData = await doctorModel.findById(docId).select('-password')
+
+        if (!docData.available) {
+            return res.json(
+                {
+                    success: false,
+                    message: "Doctor not available"
+                }
+            )
+        }
+
+        let slots_booked = docData.slots_booked
+
+        // checking for slots availability
+        if (slots_booked[slotDate]) {
+            if (slots_booked[slotDate].includes(slotTime)) {
+                return res.json(
+                    {
+                        success: false,
+                        message: "Slot not available"
+                    }
+                )
+            }
+            else {
+                slots_booked[slotDate].push(slotTime)
+            }
+        } else {
+            slots_booked[slotDate] = []
+            slots_booked[slotDate].push(slotTime)
+        }
+
+
+        // getting userData
+        const userData = await userModel.findById(userId).select('-password')
+
+        // not sending details to frontend
+        delete docData.slots_booked
+
+        const appointmentData = {
+            userId,
+            docId,
+            userData,
+            docData,
+            amount: docData.fees,
+            slotTime,
+            slotDate,
+            date: Date.now()
+        }
+
+        // saving to database
+        const newAppointment = new appointmentModel(appointmentData)
+        await newAppointment.save()
+
+        // save new slots data in docData
+        await doctorModel.findByIdAndUpdate(docId, {
+            slots_booked
+        })
+
+        res.json({
+            success: true,
+            message: "Appointment Booked"
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+export { registerUser, loginUser, getProfile, updateProfile, bookAppointment };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { registerUser, loginUser, getProfile, updateProfile } from '../controllers/userController.js'
+import { registerUser, loginUser, getProfile, updateProfile, bookAppointment } from '../controllers/userController.js'
 import authUser from '../middlewares/authUser.js'
 import upload from '../middlewares/multer.js'
 
@@ -12,8 +12,8 @@ userRouter.post('/login', loginUser)
 // route for getting user profile
 userRouter.get('/get-profile', authUser, getProfile)
 // route for updating user profile
-userRouter.post('/update-profile',upload.single('image'),authUser,updateProfile)
-
-
+userRouter.post('/update-profile', upload.single('image'), authUser, updateProfile)
+// route for book appointment
+userRouter.post('/book-appointment', authUser, bookAppointment)
 
 export default userRouter
